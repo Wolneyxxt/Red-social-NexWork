85941f93bed25978c029aa7a8f35e146580ed8e5
wolneyxxt <wolneybl2011@email.com>
2026-06-09 19:00:50 -0300

    corrige URL da API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-})
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+});
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
-})
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-export default api
+export default api;
